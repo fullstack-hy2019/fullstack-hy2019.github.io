acafab7a67a6f79c19984cc95e318087cb6672c8
Mark <mark.laatikainen@houston-inc.com>
2019-03-15 10:13:24 +0200

    Update deprecation message style

--- src/components/DeprecationMessage/DeprecationMessage.js
+++ src/components/DeprecationMessage/DeprecationMessage.js
@@ -2,17 +2,17 @@ import './DeprecationMessage.scss';
 
 import React from 'react';
 
-import { TripleBorder } from '../TripleBorder/TripleBorder';
-
 const DeprecationMessage = () => (
   <div className="deprecate">
     <div>
       <p>Tämä on 3. periodilla pidetyn, jo päättyneen kurssin sisältö.</p>
       <p>
         <strong>Jos haluat suorittaa kurssin nyt</strong>, mene osoitteeseen{' '}
-        <a href="https://fullstackopen-2019.github.io/">
-          https://fullstackopen-2019.github.io/
-        </a>
+        <span className="link">
+          <a href="https://fullstackopen-2019.github.io/">
+            https://fullstackopen-2019.github.io/
+          </a>
+        </span>
       </p>
     </div>
   </div>
